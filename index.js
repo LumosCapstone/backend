f3f1cdbe373582a6edea6791e959824cbde0f44e
Ben <lolitsaburrito@gmail.com>
2023-03-16 18:04:12 -0700

    WIP: add distance_sql fn for selecting distance in meters, progress on reserve endpoint, add distance selection example to create init schema migration

--- index.js
+++ index.js
@@ -19,6 +19,14 @@ import { RESOURCE_TYPES, API_RETURN_MESSAGES } from './js/constants.js';
 
 const metersPerMile = 1609.34;
 
+// Gives the distance in meters between the given PostGIS point and selected resources.
+// Example of point_sql: `POINT(-121.2352251 85.22345752)`
+const distance_sql = (point_sql) => `ST_DistanceSpheroid(
+  'SRID=4326;${point_sql}', 
+  resources.location, 
+  'SPHEROID["WGS 84",6378137,298.257223563]'
+) as distance_meters`;
+
 app.get('/', async (req, res) => {
   const [{ '?column?': one }] = await sql`select 1;`;
   res.send(`Hello World! Here's a number from Postgres: ${one}`);
@@ -44,16 +52,16 @@ app.get('/api/item', async (req, res) => {
 
   // TODO: join users
   const resources = await sql`
-    select id, type, name, quantity, ST_AsText(location) as location_point
+    select id, type, name, quantity, ST_AsText(location) as location_point, ${distance_sql(`POINT(${long} ${lat})`)}
     from resources
     where ST_DWithin(
       'SRID=4326;POINT(${long} ${lat})'::geography,
       resources.location::geography,
       ${max_distance * metersPerMile}
     );`;
-  
+
   // const images = await sql`(fetch one image of each resource)`;
-  
+
   // Loop through resources:
   //   get the distance between (lat, long), and the resources'
   //   `location_point` for the `distance` field in the response
@@ -69,12 +77,12 @@ app.get('/api/item/:id', async (req, res) => {
     return res.status(400).send({
       error: "BAD_REQUEST",
       message: "please provide a numeric item ID"
-    }); 
+    });
   }
 
   // TODO: join users and images
   const resources = await sql`
-    select id, type, name, quantity, ST_AsText(location) as location_point
+    select id, type, name, quantity, ST_AsText(location) as location_point, ${distance_sql(`location`)}
     from resources
     where id = ${id};`;
 
@@ -87,10 +95,36 @@ app.get('/api/item/:id', async (req, res) => {
 // POST /api/item/reserve/:id endpoint
 app.post('/api/item/reserve/:id', async (req, res) => {
 
-  // WIP
-  const result = await sql`update resources set `;
+  const { id } = req.params.id;
+  const { user_id } = req.query.user_id;
 
-  res.send();
+  if (typeof user_id != 'number' || typeof id != 'number') {
+    return res.status(400).send({
+      error: "BAD_REQUEST",
+      message: "please provide a numeric user ID and item ID"
+    });
+  }
+
+  // Get the item, but only if it currently belongs to someone
+  const items = await sql`select belongs_to
+    from resources
+    where id = ${id} and belongs_to IS NOT NULL;`;
+
+  // If we get rows back, the item is already reserved
+  if (items.length > 0) {
+    return res.status(409).send({
+      error: API_RETURN_MESSAGES.ITEM_UNAVAILABLE,
+      id
+    });
+  }
+
+  const result = await sql`update resources 
+    set belongs_to = ${user_id} where id = ${id}`;
+
+  res.status(200).send({
+    ok: API_RETURN_MESSAGES.RESERVE_SUCCESS,
+    id
+  });
 });
 
 // Start the webserver
