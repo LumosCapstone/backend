4fea176ea27d9bfc790467937366dcb91ecfc7fb
Jose Castro <chepe.castro2312@gmail.com>
2023-05-02 10:55:41 -0700

    Refactor new endpoints and solved merge issues

---
+++ api/login.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const router = express.Router();
+
+router.post('/login', async (req, res) => {
+    try {
+      const { email, password } = req.body;
+  
+      if (email == undefined || password == undefined) {
+        return res.status(400).send({
+          error: "All_PARAMETER_REQUIRED"
+        });
+      }
+  
+      const rows = await sql`SELECT * FROM users where email = ${email};`
+  
+      if (rows.length === 0) {
+        return res.status(401).json({ error: 'INVALID_EMAIL_OR_PASSWORD' });
+      }
+  
+      const user = rows[0];
+      const valid = await bcrypt.compare(password, user.password_hash);
+      if (!valid) {
+        return res.status(401).json({ error: 'INVALID_EMAIL_OR_PASSWORD' });
+      }
+  
+      const token = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN,
+      });
+      return res.status(200).json({ token, id: user.id });
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json({ error: 'INTERNAL_SERVER_ERROR' });
+    }
+});
+
+module.exports = router;
--- api/user.js
+++ api/user.js
@@ -3,41 +3,41 @@ const router = express.Router();
 
 // GET /api/user/:id endpoint
 router.get('/user/:id', async (req, res) => {
-    const user_id = parseInt(req.params.id);
-  
-    if (isNaN(user_id)) {
-      return res.status(400).send({
-        error: "BAD_REQUEST",
-        message: "please provide a numeric user ID"
+  const user_id = parseInt(req.params.id);
+
+  if (isNaN(user_id)) {
+    return res.status(400).send({
+      error: "BAD_REQUEST",
+      message: "please provide a numeric user ID"
+    });
+  }
+
+  try {
+    // Fetch the user
+    const [user] = await sql`
+      select id, name, email, phone_number
+      from users
+      where id = ${user_id};
+    `;
+
+    // If `!user`, a user under the given ID doesn't exist
+    if (!user) {
+      return res.status(404).send({
+        error: API_RETURN_MESSAGES.USER_NOT_FOUND,
+        id: user_id
       });
     }
-  
-    try {
-      // Fetch the user
-      const [user] = await sql`
-        select id, name, email, phone_number
-        from users
-        where id = ${user_id};
-      `;
-  
-      // If `!user`, a user under the given ID doesn't exist
-      if (!user) {
-        return res.status(404).send({
-          error: API_RETURN_MESSAGES.USER_NOT_FOUND,
-          id: user_id
-        });
-      }
-  
-      res.status(200).send(user);
-  
-    } catch (error) {
-      console.error(error);
-      
-      res.status(500).send({
-        error: API_RETURN_MESSAGES.INTERNAL_SERVER_ERROR,
-        message: "Internal Server Error"
-      });
-    }
-  });
+
+    res.status(200).send(user);
+
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).send({
+      error: API_RETURN_MESSAGES.INTERNAL_SERVER_ERROR,
+      message: "Internal Server Error"
+    });
+  }
+});
 
 module.exports = router;
